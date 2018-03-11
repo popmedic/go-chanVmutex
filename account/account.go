9d8174e9177807f0065ceefc09b3aed3bb279c27
Kevin Scardina <kscardina@webroot.com>
2018-03-11 10:28:10 -0600

    adding the account objects

---
+++ account/account.go
@@ -0,0 +1,109 @@
+package account
+
+import (
+	"sync"
+)
+
+type IAccount interface {
+	Sum(amounts ...float64)
+	Balance() float64
+}
+
+type ChanLockAccount struct {
+	balance float64
+	chlock  chan byte
+}
+
+type MutexAccount struct {
+	balance float64
+	lock    *sync.RWMutex
+}
+
+type ChanAccount struct {
+	sumChannel     chan []float64
+	balanceChannel chan float64
+}
+
+func NewChanLockAccount(amounts ...float64) *ChanLockAccount {
+	var balance float64 = 0
+	for _, amount := range amounts {
+		balance = balance + amount
+	}
+	return &ChanLockAccount{
+		balance: balance,
+		chlock:  make(chan byte, 1),
+	}
+}
+
+func (a *ChanLockAccount) Sum(amounts ...float64) {
+	a.chlock <- 0
+	defer func(a *ChanLockAccount) { <-a.chlock }(a)
+	for _, amount := range amounts {
+		a.balance = a.balance + amount
+	}
+}
+
+func (a *ChanLockAccount) Balance() (balance float64) {
+	a.chlock <- 0
+	defer func(a *ChanLockAccount) { <-a.chlock }(a)
+	balance = a.balance
+	return
+}
+
+func NewMutexAccount(amounts ...float64) *MutexAccount {
+	var balance float64 = 0
+	for _, amount := range amounts {
+		balance = balance + amount
+	}
+	return &MutexAccount{
+		balance: balance,
+		lock:    &sync.RWMutex{},
+	}
+}
+
+func (a *MutexAccount) Sum(amounts ...float64) {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	for _, amount := range amounts {
+		a.balance = a.balance + amount
+	}
+}
+
+func (a *MutexAccount) Balance() (balance float64) {
+	a.lock.RLock()
+	defer a.lock.RUnlock()
+	balance = a.balance
+	return
+}
+
+func NewChanAccount(amounts ...float64) *ChanAccount {
+	chanAccount := &ChanAccount{
+		sumChannel:     make(chan []float64),
+		balanceChannel: make(chan float64, 1),
+	}
+	chanAccount.balanceChannel <- 0.0
+	go chanAccount.worker()
+	chanAccount.Sum(amounts...)
+	return chanAccount
+}
+
+func (a *ChanAccount) Sum(amounts ...float64) {
+	a.sumChannel <- amounts
+}
+
+func (a *ChanAccount) Balance() float64 {
+	balance := <-a.balanceChannel
+	defer func(a *ChanAccount, b float64) { a.balanceChannel <- b }(a, balance)
+	return balance
+}
+
+func (a *ChanAccount) worker() {
+	for {
+		balance := <-a.balanceChannel
+		amounts := <-a.sumChannel
+		for _, amount := range amounts {
+			balance = balance + amount
+		}
+		a.balanceChannel <- balance
+	}
+}
