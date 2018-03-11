920fe451bb0ab56e399b505c1c310c16d131cc7d
Kevin Scardina <kscardina@webroot.com>
2018-03-11 12:51:56 -0600

    making test use concurrency

--- account/account_test.go
+++ account/account_test.go
@@ -1,17 +1,18 @@
 package account
 
 import (
+	"sync"
 	"testing"
 )
 
 func TestAccounts(t *testing.T) {
-	testAccount(t, NewChanLockAccount(4, 6), 20, 7, 3)
-	testAccount(t, NewMutexAccount(4, 6), 20, 7, 3)
-	testAccount(t, NewChanAccount(4, 6), 20, 7, 3)
+	testAccount(t, NewChanLockAccount(4, 6), 20, 4, 3, 2, 1)
+	testAccount(t, NewMutexAccount(4, 6), 20, 4, 3, 2, 1)
+	testAccount(t, NewChanAccount(4, 6), 20, 4, 3, 2, 1)
 }
 
 func testAccount(t *testing.T, a IAccount, exp float64, amounts ...float64) {
-	a.Sum(amounts...)
+	sumAmounts(a, amounts...)
 	got := a.Balance()
 	if got != exp {
 		t.Errorf("falure for type %T: expected %f got %f", a, exp, got)
@@ -33,6 +34,18 @@ func BenchmarkChanAccount(b *testing.B) {
 func benchmarkAccount(b *testing.B, a IAccount) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		a.Sum(10, 20, 30)
+		sumAmounts(a, float64(i))
 	}
+}
+
+func sumAmounts(a IAccount, amounts ...float64) {
+	wg := &sync.WaitGroup{}
+	for _, amount := range amounts {
+		wg.Add(1)
+		go func(amount float64) {
+			a.Sum(amount)
+			wg.Done()
+		}(amount)
+	}
+	wg.Wait()
 }
